a24c82bac2e85ee25691d3542cb49e9fe57bd656
Your Name <you@example.com>
2024-04-23 08:11:41 -0300

    SqlCode

--- Versiones/Sql/VS_RecuperarObjetos_Cons_sp.sql
+++ Versiones/Sql/VS_RecuperarObjetos_Cons_sp.sql
@@ -6,35 +6,54 @@ AS
 BEGIN
     IF @TipoID IS NULL OR @TipoID = 1
     BEGIN
+        WITH CTE AS (
+            SELECT
+                ROW_NUMBER() OVER (ORDER BY TABLE_SCHEMA, TABLE_NAME) AS RowNum,
+                TABLE_SCHEMA,
+                TABLE_NAME,
+                dbo.GetTableDefinitionScript(TABLE_NAME) AS definition
+            FROM
+                INFORMATION_SCHEMA.TABLES
+            WHERE
+                TABLE_TYPE = 'BASE TABLE'
+        )
         SELECT
-             CAST(ROW_NUMBER() OVER (ORDER BY TABLE_SCHEMA, TABLE_NAME) AS INT) AS ID,
+            CAST(RowNum AS INT) ID,
             TABLE_SCHEMA,
             TABLE_NAME,
-            'SELECT * FROM ' + QUOTENAME(TABLE_NAME) AS definition
+            definition
         FROM
-            INFORMATION_SCHEMA.TABLES
+            CTE
         WHERE
-            TABLE_TYPE = 'BASE TABLE'
+            RowNum BETWEEN (@PageNumber - 1) * @PageSize + 1 AND @PageNumber * @PageSize
         ORDER BY
             TABLE_SCHEMA,
-            TABLE_NAME
-        OFFSET (@PageNumber - 1) * @PageSize ROWS
-        FETCH NEXT @PageSize ROWS ONLY;
+            TABLE_NAME;
     END 
     ELSE
     BEGIN
+        WITH CTE AS (
+            SELECT 
+                ROW_NUMBER() OVER (ORDER BY ROUTINE_SCHEMA, ROUTINE_NAME) AS RowNum,
+                ROUTINE_SCHEMA AS TABLE_SCHEMA,
+                ROUTINE_NAME AS TABLE_NAME,
+                (SELECT ISNULL(definition, 'Encriptado') FROM sys.all_sql_modules WHERE object_id = OBJECT_ID(ROUTINE_NAME)) AS definition
+            FROM 
+                INFORMATION_SCHEMA.ROUTINES
+            WHERE 
+                ROUTINE_TYPE = 'PROCEDURE'
+        )
         SELECT 
-            CAST(ROW_NUMBER() OVER (ORDER BY ROUTINE_SCHEMA, ROUTINE_NAME) AS INT) AS ID,
-            ROUTINE_SCHEMA AS TABLE_SCHEMA,
-            ROUTINE_NAME AS TABLE_NAME,
-            (SELECT ISNULL(definition, 'Encriptado') FROM sys.all_sql_modules WHERE object_id = OBJECT_ID(ROUTINE_NAME)) AS definition
+            CAST(RowNum AS INT) ID,
+            TABLE_SCHEMA,
+            TABLE_NAME,
+            definition
         FROM 
-            INFORMATION_SCHEMA.ROUTINES
+            CTE
         WHERE 
-            ROUTINE_TYPE = 'PROCEDURE'
+            RowNum BETWEEN (@PageNumber - 1) * @PageSize + 1 AND @PageNumber * @PageSize
         ORDER BY 
-            ROUTINE_SCHEMA, ROUTINE_NAME
-        OFFSET (@PageNumber - 1) * @PageSize ROWS
-        FETCH NEXT @PageSize ROWS ONLY;
+            TABLE_SCHEMA, 
+            TABLE_NAME;
     END
 END
